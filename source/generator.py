fc16d65c0d01e82d12c8fbf541f62c5acca214f5
Ramb Memburg <ramb.memburg@gmail.com>
2024-06-22 22:41:34 -0400

    threshold

--- source/generator.py
+++ source/generator.py
@@ -1,18 +1,54 @@
 import argparse
 from PIL import Image
 
+# define constants
+COMMENT_WIDTH = 13
+COMMENT_HEIGHT = 25
+FINAL_WIDTH = 2 * COMMENT_WIDTH
+FINAL_HEIGHT = 4 * COMMENT_HEIGHT
+BRAILLE_CHARACTERS = "".join(
+    [
+        "⡀⡁⡂⡃⡄⡅⡆⡇⡈⡉⡊⡋⡌⡍⡎⡏",
+        "⡐⡑⡒⡓⡔⡕⡖⡗⡘⡙⡚⡛⡜⡝⡞⡟",
+        "⡠⡡⡢⡣⡤⡥⡦⡧⡨⡩⡪⡫⡬⡭⡮⡯",
+        "⡰⡱⡲⡳⡴⡵⡶⡷⡸⡹⡺⡻⡼⡽⡾⡿",
+        "⢀⢁⢂⢃⢄⢅⢆⢇⢈⢉⢊⢋⢌⢍⢎⢏",
+        "⢐⢑⢒⢓⢔⢕⢖⢗⢘⢙⢚⢛⢜⢝⢞⢟",
+        "⢠⢡⢢⢣⢤⢥⢦⢧⢨⢩⢪⢫⢬⢭⢮⢯",
+        "⢰⢱⢲⢳⢴⢵⢶⢷⢸⢹⢺⢻⢼⢽⢾⢿",
+        "⣀⣁⣂⣃⣄⣅⣆⣇⣈⣉⣊⣋⣌⣍⣎⣏",
+        "⣐⣑⣒⣓⣔⣕⣖⣗⣘⣙⣚⣛⣜⣝⣞⣟",
+        "⣠⣡⣢⣣⣤⣥⣦⣧⣨⣩⣪⣫⣬⣭⣮⣯",
+        "⣰⣱⣲⣳⣴⣵⣶⣷⣸⣹⣺⣻⣼⣽⣾⣿",
+    ]
+)
 
-def get_pixels(image_path: str):
-    # Open an image file
-    image = Image.open(image_path)
 
-    # Get the image dimensions
+def threshold(image: Image, theta: int) -> None:
+    # get image dimansions
     width, height = image.size
 
-    # Load image data
+    # get pixels
     pixels = image.load()
 
-    print(width, height)
+    for x in range(width):
+        for y in range(height):
+            # get rgb values from the current pixel
+            r, g, b = pixels[x, y]
+            avg = (r + g + b) / 3
+
+            if avg < theta:
+                # set the pixel to black
+                pixels[x, y] = (0, 0, 0)
+            else:
+                # set the pixel to white
+                pixels[x, y] = (255, 255, 255)
+
+
+def crop(image: Image) -> None:
+    # width = 26 13
+    # height = 100 50
+    pass
 
 
 if __name__ == "__main__":
@@ -22,9 +58,8 @@ if __name__ == "__main__":
     )
 
     # create optional arguments
-    parser.add_argument("--c", type=str, default="centre", help="Crop mode")
-    parser.add_argument("--o", type=str, default="output.txt", help="Output name")
-    parser.add_argument("--t", type=str, default="output.txt", help="Thresholding mode")
+    parser.add_argument("--t", type=int, default=100, help="Thresholding range")
+    parser.add_argument("--o", type=str, default="ascii.png", help="Output name")
 
     # create required arguments
     parser.add_argument("image_path", type=str, help="Path to the input image")
@@ -32,5 +67,12 @@ if __name__ == "__main__":
     # parse arguments
     args = parser.parse_args()
 
-    # start the image processing
-    get_pixels(args.image_path)
+    # load the image
+    image = Image.open(args.image_path)
+
+    # perform the threshold to the image
+    threshold(image, args.t)
+    crop(image)
+
+    # save the image
+    image.save(args.o)
